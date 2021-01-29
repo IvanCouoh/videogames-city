678fc71479457905c9e52f0f3a0fbdd9c269aed7
javier_herrera <javier.ivancouoh@gmail.com>
2021-01-29 15:12:06 -0600

    creacion de bd en servidor

--- public/conexion/conexion.js
+++ public/conexion/conexion.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 
 var conexion=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'videogame'
+    host:'bfsjlxhv01mtogsv1lhq-mysql.services.clever-cloud.com',
+    user:'udtjexhtguzaahna',
+    password:'OhpUqiJjZpzP8FMG8lTw',
+    database:'bfsjlxhv01mtogsv1lhq'
 });
 
 conexion.connect(function (error){
